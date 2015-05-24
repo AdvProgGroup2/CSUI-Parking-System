5a83ea6b2fa7fb85b9d9ae4f15a81dae420f3447
Hakeem <hakeemrdaud@gmail.com>
2015-05-24 16:49:45 +0700

    lot database update
    added 30 parking space in the database

--- lot.sql
+++ lot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2015 at 11:26 AM
+-- Generation Time: May 24, 2015 at 11:48 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -31,6 +31,42 @@ CREATE TABLE IF NOT EXISTS `lot` (
   `status` tinyint(1) NOT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lot`
+--
+
+INSERT INTO `lot` (`id`, `status`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0),
+(26, 0),
+(27, 0),
+(28, 0),
+(29, 0),
+(30, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
