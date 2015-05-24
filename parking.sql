23241c6613c0ad2049311b9b4b6457a9afb893be
Hakeem <hakeemrdaud@gmail.com>
2015-05-24 17:23:01 +0700

    update parking database
    inside has 3 table: guest, parking lot, and member. member has new
    column "email". added a new member with staff as the role.

--- parking.sql
+++ parking.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2015 at 12:24 PM
+-- Generation Time: May 24, 2015 at 12:21 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -19,8 +19,73 @@ SET time_zone = "+00:00";
 --
 -- Database: `parking`
 --
-CREATE DATABASE IF NOT EXISTS `parking` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `parking`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest`
+--
+
+CREATE TABLE IF NOT EXISTS `guest` (
+  `guest` char(30) NOT NULL,
+  `phone_number` bigint(15) NOT NULL,
+  UNIQUE KEY `guest` (`guest`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `guest`
+--
+
+INSERT INTO `guest` (`guest`, `phone_number`) VALUES
+('Naning', 81257386012);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lot`
+--
+
+CREATE TABLE IF NOT EXISTS `lot` (
+  `id` int(2) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lot`
+--
+
+INSERT INTO `lot` (`id`, `status`) VALUES
+(1, 0),
+(2, 0),
+(3, 0),
+(4, 0),
+(5, 0),
+(6, 0),
+(7, 0),
+(8, 0),
+(9, 0),
+(10, 0),
+(11, 0),
+(12, 0),
+(13, 0),
+(14, 0),
+(15, 0),
+(16, 0),
+(17, 0),
+(18, 0),
+(19, 0),
+(20, 0),
+(21, 0),
+(22, 0),
+(23, 0),
+(24, 0),
+(25, 0),
+(26, 0),
+(27, 0),
+(28, 0),
+(29, 0),
+(30, 0);
 
 -- --------------------------------------------------------
 
@@ -34,6 +99,7 @@ CREATE TABLE IF NOT EXISTS `member` (
   `password` varchar(30) NOT NULL,
   `full_name` varchar(40) NOT NULL,
   `role` varchar(255) NOT NULL,
+  `email` varchar(30) NOT NULL,
   PRIMARY KEY (`npm`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -42,9 +108,11 @@ CREATE TABLE IF NOT EXISTS `member` (
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`npm`, `username`, `password`, `full_name`, `role`) VALUES
-('1306387941', 'tsabita.cyavrilla', 'tsabtsab', 'Tsabita Cyavrilla', 'Student'),
-('1306439251', 'yohana.fransiska', 'yaya', 'Yohana Fransiska', 'Student');
+INSERT INTO `member` (`npm`, `username`, `password`, `full_name`, `role`, `email`) VALUES
+('1306387916', 'hakeem', '1234', 'Hakeem', 'Student', 'hakeem.radiansyah@ui.ac.id'),
+('1306387941', 'tsabita.cyavrilla', 'tsabtsab', 'Tsabita Cyavrilla', 'Student', 'tsabita.cyavrilla.ui.ac.id'),
+('1306439251', 'yohana.fransiska', 'yaya', 'Yohana Fransiska', 'Student', 'yohana.fransiska@ui.ac.id'),
+('4543918572', 'ed', 'pass', 'Eddarius', 'Staff', 'eddarius@cs.ui.ac.id');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
